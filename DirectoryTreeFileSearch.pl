643df65b654a64111854c35f65bde26e94193ce1
thefenry <thefenry@gmail.com>
2014-06-23 16:34:23 -0700

    Added the condition
    This condition will not allow the recursion to occur if the directory
    exists.

--- DirectoryTreeFileSearch.pl
+++ DirectoryTreeFileSearch.pl
@@ -2,17 +2,20 @@ use File::Find;
 
 @fileLocations;
 
-find (
-{
-	wanted => \&findfiles,
-
-	}, 'C:\Users\thefenry\Desktop\testdirectory' #this is the location of the directory root
-	);
+find ( 
+	{	wanted => \&findfiles,	}, 'C:\Source\ServerEPS\_BASELINE_\DotNet4x' #this is the location of the directory root
+);
 
 sub findfiles{
-	push(@fileLocations, $File::Find::name) if /test1.cs$/;
+	if (-d and $_ eq "3rdParty") {   #This line of code will exclude the directory 3rdparty if it is found. The
+		$File::Find::prune = 1					#directory will be ignored along with files in it
+	}
+	push(@fileLocations, $File::Find::name) if /AssemblyInfo.cs$/;
 }
+ foreach (@fileLocations){
+ 	print "$_\n"
+ }
 
-# print "Size: ", scalar @fileLocations, "\n"; #This prints the size just to make sure that it 
+print "Size: ", scalar @fileLocations, "\n"; #This prints the size just to make sure that it 
 																							#is as expected when developing
 exit;
